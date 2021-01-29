03aa8e7be9483e34b36efc9e7b72b2c8ad02f2f1
houbly <h.blyden@gmail.com>
2021-01-29 11:55:07 +0000

    Update icon component

--- src/components/shared/Icon/Icon.js
+++ src/components/shared/Icon/Icon.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import s from './Icon.module.scss';
+
 // Due to weird Protocol errors with external SVGs the svg use doesn't work well with production builds
 // So we Ajax the SVG in with a snippet at the bottom of public/index.html
 
 // Icons can be found at: https://designsystem.wmnetwork.co.uk/styles/icons/
 
-const Icon = ({ className, iconName }) => {
+const Icon = ({ className, iconName, size }) => {
   return (
-    <svg className={className}>
+    <svg className={`${s.icon} ${className}`} width={`${size}px`} height={`${size}px`}>
       <use xlinkHref={`#wmnds-${iconName}`} href={`#wmnds-${iconName}`} />
     </svg>
   );
@@ -17,10 +19,12 @@ const Icon = ({ className, iconName }) => {
 Icon.propTypes = {
   iconName: PropTypes.string.isRequired,
   className: PropTypes.string,
+  size: PropTypes.number,
 };
 
 Icon.defaultProps = {
   className: null,
+  size: 20,
 };
 
 export default Icon;
