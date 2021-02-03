df734f0a281a95bfbb1298b3a5f7b6e039ebdbcb
houbly <h.blyden@gmail.com>
2021-02-03 19:11:24 +0000

    toggle map access menu visiblility

--- src/components/RailZoneFinder/Map/Map.js
+++ src/components/RailZoneFinder/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useContext, createRef } from 'react';
+import React, { useContext, useState, createRef } from 'react';
 import RailZoneMap from './RailZoneMap';
 import Icon from '../../shared/Icon/Icon';
 import AccessIcon from '../../shared/Icon/AccessIcon';
@@ -11,6 +11,7 @@ import { AutoCompleteContext } from 'globalState';
 
 const Map = () => {
   const [autoCompleteState] = useContext(AutoCompleteContext);
+  const [showKey, setShowKey] = useState(false);
   const { selectedStations } = autoCompleteState;
   const mapRef = createRef();
 
@@ -44,39 +45,46 @@ const Map = () => {
   return (
     <div className={s.mapContainer}>
       <RailZoneMap mapRef={mapRef} />
-      <Button
-        btnClass={`wmnds-btn--primary ${s.showKeyBtn}`}
-        text="Show parking and accessibility"
-        iconRight="general-chevron-right"
-      />
-      <div className={`wmnds-p-md bg-white ${s.accessMenu}`}>
-        <div className="wmnds-grid wmnds-grid--justify-between">
-          <h3 className="wmnds-col-auto">Show parking and accessibility</h3>
-          <div className="wmnds-col-auto">
-            <Button
-              btnClass={`wmnds-btn--link ${s.hideKeyBtn}`}
-              text="Hide"
-              iconRight="general-chevron-right"
-            />
+      {!showKey ? (
+        <Button
+          btnClass={`wmnds-btn--primary ${s.showKeyBtn}`}
+          text="Show parking and accessibility"
+          iconRight="general-chevron-right"
+          onClick={() => setShowKey(true)}
+        />
+      ) : (
+        <div className={`wmnds-p-md bg-white ${s.accessMenu}`}>
+          <div className="wmnds-grid wmnds-grid--justify-between">
+            <h3 className="wmnds-col-auto">Show parking and accessibility</h3>
+            <div className="wmnds-col-auto">
+              <Button
+                btnClass={`wmnds-btn--link ${s.hideKeyBtn}`}
+                text="Hide"
+                iconRight="general-chevron-right"
+                onClick={() => setShowKey(false)}
+              />
+            </div>
           </div>
+          <Checkbox classes="wmnds-m-b-sm">
+            <div style={{ display: 'flex' }}>
+              <AccessIcon type="full" className="wmnds-m-r-sm" /> Stations with full step-free
+              access
+            </div>
+          </Checkbox>
+          <Checkbox classes="wmnds-m-b-sm">
+            <div style={{ display: 'flex' }}>
+              <AccessIcon type="part" className="wmnds-m-r-sm" /> Stations with part step-free
+              access
+            </div>
+          </Checkbox>
+          <Checkbox classes="wmnds-m-b-sm">
+            <div style={{ display: 'flex' }}>
+              <Icon iconName="general-parking" className="wmnds-m-r-sm" color="cta" /> Stations with
+              parking
+            </div>
+          </Checkbox>
         </div>
-        <Checkbox classes="wmnds-m-b-sm">
-          <div style={{ display: 'flex' }}>
-            <AccessIcon type="full" className="wmnds-m-r-sm" /> Stations with full step-free access
-          </div>
-        </Checkbox>
-        <Checkbox classes="wmnds-m-b-sm">
-          <div style={{ display: 'flex' }}>
-            <AccessIcon type="part" className="wmnds-m-r-sm" /> Stations with part step-free access
-          </div>
-        </Checkbox>
-        <Checkbox classes="wmnds-m-b-sm">
-          <div style={{ display: 'flex' }}>
-            <Icon iconName="general-parking" className="wmnds-m-r-sm" color="cta" /> Stations with
-            parking
-          </div>
-        </Checkbox>
-      </div>
+      )}
     </div>
   );
 };
