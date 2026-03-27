2d61d36587bced44845f5e2f6f16121cc3c20c06
Nathan Rogan <nathan.rogan@wmca.org.uk>
2026-03-27 10:10:19 +0000

    fix lint issues

--- src/components/shared/Radios/Radio/Radio.js
+++ src/components/shared/Radios/Radio/Radio.js
@@ -1,41 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import dompurify from 'dompurify';
-
-// Import contexts
-import { FormContext } from 'globalState';
 
 import s from './Radio.module.scss';
 
 const { sanitize } = dompurify;
 
-const Radio = ({ name = '', onChange, text, value, info }) => {
-  const [formState] = useContext(FormContext); // Get the state/dispatch of form data from FormContext
-
+function Radio({ name = '', onChange, text, value, info }) {
   return (
-    <>
-      <label className={`${s.radioContainer} wmnds-fe-radios__container`}>
-        <div className={s.text} dangerouslySetInnerHTML={{ __html: sanitize(text) }} />
-        <input
-          className={`${s.radio} wmnds-fe-radios__input`}
-          value={value}
-          name={name}
-          type="radio"
-          onChange={onChange}
-          defaultChecked={formState[name] && formState[name] === value}
+    <label className={`${s.radioContainer} wmnds-fe-radios__container`}>
+      <div className={s.text} dangerouslySetInnerHTML={{ __html: sanitize(text) }} />
+      <input
+        className={`${s.radio} wmnds-fe-radios__input`}
+        value={value}
+        name={name}
+        type="radio"
+        onChange={onChange}
+      />
+      {info && (
+        <div
+          className={`${s.insetText} wmnds-inset-text wmnds-m-t-xs`}
+          dangerouslySetInnerHTML={{ __html: sanitize(info) }}
         />
-        {info && (
-          <div
-            className={`${s.insetText} wmnds-inset-text wmnds-m-t-xs`}
-            dangerouslySetInnerHTML={{ __html: sanitize(info) }}
-          />
-        )}
-        <span className="wmnds-fe-radios__checkmark" />
-      </label>
-    </>
+      )}
+      <span className="wmnds-fe-radios__checkmark" />
+    </label>
   );
-};
+}
 
 Radio.propTypes = {
   name: PropTypes.string,
